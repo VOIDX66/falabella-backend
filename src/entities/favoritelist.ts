0805eaadce431b6b3f5170ba394d3c8fcef34cee
Jaider R <jaiderrendon66@gmail.com>
2025-04-15 20:26:34 -0500

    Corrigiendo entidad de productos favoritos

--- src/entities/favoritelist.ts
+++ src/entities/favoritelist.ts
@@ -1,41 +1,40 @@
 import {
-    BaseEntity,
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    ManyToOne,
-    OneToMany,
-    CreateDateColumn,
-    UpdateDateColumn,
-    JoinColumn,
-  } from "typeorm";
-  import { User } from "./user";
-  import { FavoriteProduct } from "./favoriteproduct";
-  
-  @Entity()
-  export class FavoriteList extends BaseEntity {
-    @PrimaryGeneratedColumn()
-    id_favorite_list: number;
-  
-    @Column()
-    name: string;
-  
-    @Column({ default: false })
-    is_default: boolean;
-  
-    @ManyToOne(() => User, (user) => user.favoriteLists, { onDelete: "CASCADE" })
-    @JoinColumn({ name: "user_id" })
-    user: User;
-  
-    @OneToMany(() => FavoriteProduct, (fp) => fp.favoriteList, {
-      cascade: true,
-    })
-    products: FavoriteProduct[];
-  
-    @CreateDateColumn()
-    created_at: Date;
-  
-    @UpdateDateColumn()
-    updated_at: Date;
-  }
-  
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  OneToMany,
+  CreateDateColumn,
+  UpdateDateColumn,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./user";
+import { FavoriteProduct } from "./favoriteproduct";
+
+@Entity()
+export class FavoriteList extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id_favorite_list: number;
+
+  @Column()
+  name: string;
+
+  @Column({ default: false })
+  is_default: boolean;
+
+  @ManyToOne(() => User, { onDelete: "CASCADE" })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @OneToMany(() => FavoriteProduct, (favoriteProduct) => favoriteProduct.favoriteList, {
+    cascade: true,
+  })
+  products: FavoriteProduct[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
