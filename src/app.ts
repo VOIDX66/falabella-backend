20d825e57ee67117571a3cb8b2615c36bc4cd7c8
Jaider R <jaiderrendon66@gmail.com>
2025-04-15 20:23:57 -0500

    Agregando las rutas a la app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import user_routes from "./routes/user.routes"
 import product_routes from "./routes/product.routes"
 import cart_routes from "./routes/cart.routes"
+import favorite_routes from "./routes/favorite.routes"
 import path from "path";
 
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(user_routes);
 app.use(product_routes);
 app.use(cart_routes);
+app.use(favorite_routes);
 // Servir archivos estáticos desde la carpeta "images"
 app.use("/images", express.static(path.join(__dirname, "images")));
 
