adb2b0f6764c2f6a50457ed7eaf08816a567a3dd
Jaider R <jaiderrendon66@gmail.com>
2025-05-06 11:45:40 -0500

    Actualizamos rutas

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { getProductById, getProductsBySection, getProductsByCategory, getProductsBySubcategory, getFilteredProducts } from "../controllers/product.controllers";
+import { getProductById, getProductsBySection, getProductsByCategory, getProductsBySubcategory, getFilteredProducts, createProductComment, getProductReviews, giveLikeOrDislike } from "../controllers/product.controllers";
+import { upload } from "../middlewares/upload";
 
 const router = Router();
 
 // Obtener producto en especifico (usando Id)
 router.get("/product/:id", getProductById);
+
+router.get("/reviews_product/:productId", getProductReviews);
 
 // Obtener productos por sección (usando Slug)
 //router.get("/section/:sectionSlug", getProductsBySection);
@@ -26,5 +29,10 @@ router.post("/collection/:sectionSlug/:categorySlug", getFilteredProducts);
 
 // Filtrar productos por subcategoría
 router.post("/collection/:sectionSlug/:categorySlug/:subcategorySlug", getFilteredProducts);
+
+// Subir imagenes de los comentarios
+router.post("/comments/add_review", upload.array("photos", 6), createProductComment);
+
+router.post("/comments/like_dislike", giveLikeOrDislike);
 
 export default router;
